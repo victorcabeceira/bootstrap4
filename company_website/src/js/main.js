37ee690566b939a45f6d0c5635d4b80fb473c265
Victor Fellipe <victorfgcabeceira@gmail.com>
2017-11-21 19:02:46 -0200

    Creating icon boxes and testimonials sections with slick slider

--- company_website/src/js/main.js
+++ company_website/src/js/main.js
@@ -24,3 +24,9 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event){
   event.preventDefault();
   $(this).ekkoLightbox();
 });
+
+$('.slider').slick({
+  infinite: true,
+  slideToShow:1,
+  slideToScroll:1
+});
